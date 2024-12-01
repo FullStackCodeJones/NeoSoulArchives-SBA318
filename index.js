676ecc4f9896fef4258908cbc61d8c80d58907ac
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-12-01 11:18:11 -0500

    Second Commit for NeoSoul Archives

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Uses method override to handle the PUT/DELETE requests
 app.use(methodOverride("_method"));
+
+//Static Files
+//Serves the static files like CSS, and Images
+app.use(express.static(path.join(__dirname, "public")));
+
+//Setting up EJS as the template engine for dynamic HTML rendering
+app.set("view engine", "ejs");
+
+//Routes
+
+//Home Route: Displays a Welcome Page
+app.get("/", (req, res) => {
+  res.render("index", { title: "Neo-Soul & Spoken Word Archive" });
+});
+
+//Start The Server On Port 3000
+const PORT = 3000; //Defines The Port Number
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
